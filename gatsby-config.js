f5cec2546706fa9fbf170d97965d50650483c1ea
Espoir Murhabazi <espoir.mur@gmail.com>
2020-01-24 11:19:08 +0200

    - Task Completed :  Complete the deployement
    - Next Task : make the domain name work

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,6 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    { pathPrefix: "/reponame" },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
